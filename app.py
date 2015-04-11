f418df7ebe6a1ee84dc6ddb56ad6c509bb92eb99
Anthony Pleshek <anthony.pleshek@gmail.com>
2015-04-11 15:58:02 -0500

    Doing score sorting in python like a decent person

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from operator import attrgetter
 import sqlite3 as lite
 import re
 import copy
@@ -59,23 +60,34 @@ def home():
                 r = results[ri]
                 if reslen(r) == i:
                     current_r = copy.copy(r)
-                    r.append([q[i],0])
+                    # r.append([q[i],0])
+                    r.append({"w":str(q[i]),"score":0})
                     for w in words:
                         if reslen(current_r)+len(w) <= len(q) and w == q[i:i+len(w)].upper():
                             new = copy.copy(current_r)
-                            new.append([w,len(w)])
+                            new.append({"w":str(w),"score":len(w)})
                             results.append(new)
 
-    return render_template('main.html',results = json.dumps(results), query=q, words=json.dumps(words))
+    for idx in results:
+        print idx;
+    # return render_template('main.html',results = json.dumps(results), query=q, words=json.dumps(words))
+    return render_template('main.html',results = sorted(results,key=lambda x : resscore(x), reverse=True), query=q, words=words)
 
 def reslen(result):
     length = 0
     if result is not None:
         for r in result:
-            length += len(r[0])
+            length += len(r['w'])
     else:
         length = -1
     return length
+
+def resscore(result):
+    score = 0.0
+    for r in result:
+        score += r['score']
+    score = score/len(result)
+    return score
 
 if __name__ == '__main__':
     app.debug=True
